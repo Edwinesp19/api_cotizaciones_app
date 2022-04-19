6727326784c8d872c080518a11ba1a8983275239
Edwin de Jesús Espinal <edwinespinal1999@gmail.com>
2022-04-18 22:09:43 -0400

    se agregó la ruta de categorias por productos

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 
 // Routes
-//app.use(require('./routes/products'));
+app.use(require('./routes/products'));
 app.use(require('./routes/orders'));
+app.use(require('./routes/ordersdetails'));
+app.use(require('./routes/customers'));
 
 // Starting the server
 app.listen(app.get('port'), () => {
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
+
 const mysqlConnection  = require('../database.js');
 
 // GET all Employees
-router.get('/', (req, res) => {
-  mysqlConnection.query('SELECT * FROM categories JOIN products ON categories.id = products.category_id order BY  categories.id asc', (err, rows, fields) => {
+let data = ""
+router.get('/categoriesproducts', (req, res) => {
+  
+  mysqlConnection.query('SELECT * FROM categories',async  (err, rows) => {
 
 
+    let result_product = rows.map(async ({ id, name}) => {
+
+      const data = function(){
+        return new Promise(function(resolve, reject){
+          mysqlConnection.query(`SELECT categories.id AS id_categoria, products.id AS id_product, products.name AS producto, products.price AS precio, products.stock AS stock FROM categories JOIN products ON ${id} = products.category_id order BY  categories.id asc`, async(err, rows2, fields)=>{
+            const hj = {
+              id_categorie: id,
+              name_categories: name,
+              products: rows2.filter((product)=> product.id_categoria === id ? product : ''  )
+            }
+            resolve(hj)
+          })
+        })
+      }
+     
+    const result = await data()
+    const res2 = [result]
+    return res2
+    })
+    const rs = await Promise.all(result_product)
+    res.json(rs)
+    
+  });  
+});
 
 
+router.get('/products', (req, res) => {
+  mysqlConnection.query('SELECT categories.id AS id_categoria, categories.name AS categoria, products.id AS id_producto, products.name AS producto, products.price AS precio, products.stock AS stock FROM categories JOIN products ON categories.id = products.category_id order BY  categories.id asc', (err, rows, fields) => {
+
+console.log(rows)
+    const result_product = rows.map(({ category_id }) => ( { category_id } ))
     if(!err) {
-      res.json(rows);
+      res.json(result_product);
     } else {
       console.log(err);
     }
@@ -19,7 +51,7 @@ router.get('/', (req, res) => {
 });
 
 // GET An Employee
-router.get('/:id', (req, res) => {
+router.get('/products/:id', (req, res) => {
   const { id } = req.params; 
   mysqlConnection.query('SELECT * FROM products WHERE id = ?', [id], (err, rows, fields) => {
     if (!err) {
